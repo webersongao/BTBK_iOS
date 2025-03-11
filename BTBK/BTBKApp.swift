555b8fe7963f011fba69fb43c0947c9e53abbfd7
WebersonGao <gao@btbk.org>
2025-03-11 19:09:29 +0800

    初始化项目

---
+++ BTBK/BTBKApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTBKApp.swift
+//  BTBK
+//
+//  Created by WebersonGao on 2025/3/11.
+//
+
+import SwiftUI
+
+@main
+struct BTBKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BTBKTests/BTBKTests.swift
@@ -0,0 +1,17 @@
+//
+//  BTBKTests.swift
+//  BTBKTests
+//
+//  Created by WebersonGao on 2025/3/11.
+//
+
+import Testing
+@testable import BTBK
+
+struct BTBKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
